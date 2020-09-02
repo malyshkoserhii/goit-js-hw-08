bd20779f45371ea2c4ccacc6afeddab482d1ff1b
Serhii Malyshko <malyshkosergiy@gmail.com>
2020-09-03 01:52:44 +0300

    task has been completed

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 import images from "./gallery-items.js";
 
+const galleryRef = document.querySelector(".js-gallery");
+const lightbox = document.querySelector('.js-lightbox');
+const modalWindow = document.querySelector('.lightbox__image');
+const closeModalBtn = document.querySelector('button[data-action="close-lightbox"]');
+const backDrop = document.querySelector('.lightbox__content');
+
 const createGallery = (image) => {
     const galleryItemRef = document.createElement("li");
     galleryItemRef.classList.add('gallery__item');
@@ -11,6 +17,7 @@ const createGallery = (image) => {
     const galleryImageRef = document.createElement('img');
     galleryImageRef.classList.add("gallery__image");
     galleryImageRef.src = image.preview;
+    galleryImageRef.dataset.source = image.original;
     galleryImageRef.alt = image.description;
 
     galleryLinkRef.appendChild(galleryImageRef);
@@ -20,7 +27,40 @@ const createGallery = (image) => {
 };
 
 const galleryCards = images.map((image) => createGallery(image));
-
-const galleryRef = document.querySelector(".js-gallery");
-
 galleryRef.append(...galleryCards);
+
+const onImageClick = (event) => {
+    if (event.target.nodeName !== "UL") {
+        event.preventDefault();
+        onOpenModal();
+        return;
+    }
+};
+
+const onOpenModal = () => {
+    window.addEventListener('keydown', onPressEscape);
+    lightbox.classList.add('is-open');
+    modalWindow.src = event.target.dataset.source;
+};
+
+const onCloseModal = () => {
+    window.removeEventListener('keydown', onPressEscape);
+    lightbox.classList.remove('is-open');
+    modalWindow.src = '';
+};
+
+const onPressEscape = (event) => {
+    if (event.code === 'Escape') {
+        onCloseModal();
+    }
+}
+
+const onBackDropClick = (event) => {
+    if (event.target === event.currentTarget) {
+        onCloseModal();
+    }
+}
+
+galleryRef.addEventListener('click', onImageClick);
+closeModalBtn.addEventListener('click', onCloseModal);
+backDrop.addEventListener('click', onBackDropClick);
